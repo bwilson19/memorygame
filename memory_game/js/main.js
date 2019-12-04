d43951765889fd9aca6f8eec46bc68dea58cf031
Brendan Wilson <brendanwilson@Brendans-MacBook-Air.local>
2019-12-03 16:14:51 -0800

    added score and reload button

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -5,9 +5,29 @@ suit: "hearts",
 cardImage: "images/queen-of-hearts.png",
 },
 {
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png",
+},
+{
 rank: "queen",
 suit: "diamonds",
 cardImage: "images/queen-of-diamonds.png",
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png",
+},
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png",
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png",
 },
 {
 rank: "king",
@@ -21,12 +41,19 @@ cardImage: "images/king-of-diamonds.png",
 }
 ];
 
-var cardsInPlay = [];
+var cardsInPlayRank = [];
+var cardsInPlaySuit = [];
 
+var previousScore = 0 + sessionStorage.getItem("previousScoreMemory");
+var previousScoreNumber = parseInt(previousScore, 10);
+  document.getElementById("score").innerHTML = "Score: " + previousScoreNumber + " Wins";
 
 function checkForMatch (){
-if (cardsInPlay[0] === cardsInPlay[1]) {
+if (cardsInPlayRank[0] === cardsInPlayRank[1] && cardsInPlaySuit[0] === cardsInPlaySuit[1]) {
   document.getElementById("result").innerHTML = "You found a match! Congrats!"; 
+  var scorePlus = 1 + previousScoreNumber;
+  document.getElementById("score").innerHTML = "Score: " + scorePlus + " Wins";
+  sessionStorage.setItem("previousScoreMemory", scorePlus);
 } else {
   document.getElementById("result").innerHTML = "The cards did not match. Sorry, try again!"; 
 }
@@ -34,15 +61,18 @@ if (cardsInPlay[0] === cardsInPlay[1]) {
 
 function flipCard(){
 var cardId= this.getAttribute('data-id');	
-cardsInPlay.push(cards[cardId].rank);
+cardsInPlayRank.push(cards[cardId].rank);
+cardsInPlaySuit.push(cards[cardId].suit);
 
 console.log("User flipped " + cards[cardId].rank);
 console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
 this.setAttribute("src", cards[cardId].cardImage);
 
-if (cardsInPlay.length === 2){
+if (cardsInPlayRank.length === 2){
 checkForMatch();
+createReload();
+document.getElementById("result").scrollIntoView();
 }
 }
 
@@ -56,3 +86,33 @@ for (var i = 0; i < cards.length; i++) {
  }
 }
 createBoard();
+
+
+function createReload(){
+  if (cardsInPlayRank[0] === cardsInPlayRank[1] && cardsInPlaySuit[0] === cardsInPlaySuit[1]) {
+	var reloadButtonWin = document.createElement("button");
+	document.getElementById('reload').appendChild(reloadButtonWin);
+	document.getElementById("reload").innerHTML = "<button >Play Again?</button>";
+}else{
+  var reloadButtonLoss = document.createElement("button");
+  document.getElementById('reload').appendChild(reloadButtonLoss);
+  document.getElementById("reload").innerHTML = "<button >Try Again?</button>";
+}
+}
+
+function createDesc(){
+var descWindow = document.createElement('p'); 
+document.getElementById("desc").innerHTML = "<p>Concentration, also known as Match Match, Memory, Pelmanism, Shinkei-suijaku, Pexeso or simply Pairs, is a card game in which all of the cards are laid face down on a surface and two cards are flipped face up over each turn.</p><p>The object of the game is to turn over pairs of matching cards. Give it a try!</p>";
+document.getElementById('desc').appendChild(descWindow);
+} 
+
+document.getElementById('instruct').addEventListener('click', createDesc);
+
+function reloadGame(e) {
+        location.reload();
+      }
+
+document.getElementById("reload").addEventListener('click', reloadGame);
+
+
+
